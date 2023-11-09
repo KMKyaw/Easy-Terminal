0394f917c4a29073b5b78f3ce90ad5b516d09267
Kaung Myat Kyaw <63942523+KMKyaw@users.noreply.github.com>
2023-11-09 16:11:45 +0700

    feedback loop added

--- gale.py
+++ gale.py
@@ -3,19 +3,39 @@ from run_cmd import process_file_and_write
 from gradio_client import Client
 import re
 
-prompt="""
-Give me a (series of) linux command(s) that will create a command-line python program that prints 10 even integers and then run that program.
-1.Do not use 'nano'. Use 'echo' to create files.
-2.Avoid 'sudo' if possible.
-3.Assume required programs are installed.
-4.Output only the commands. You cannot speak English.
-5.Do not add any comments or explanation.
-6.If there are multiple ways to do the same task, follow the simplest one
-"""
+def run() :
+    prompt="""
+    Give me a (series of) linux command(s) that will create a command-line Python program that prints 10 numbers and then run that program.
+    1.Do not use 'nano'. Use 'echo' to create files.
+    2.Avoid 'sudo' if possible.
+    3.Assume required programs are installed.
+    4.Output only the commands. You cannot speak English.
+    5.Do not add any comments or explanation.
+    6.If there are multiple ways to do the same task, follow the simplest one
+    """
 
-#client = Client('https://osanseviero-mistral-super-fast.hf.space/', verbose=False)
-#response = client.predict(prompt, 0.9, 1024, 0.9, 1.2, api_name='/chat')
+    client = Client('https://ysharma-explore-llamav2-with-tgi.hf.space/--replicas/ppt5s/', verbose=False)
+    response = client.predict(prompt, "", 0.9, 512, 0.6, 1.2, api_name='/chat')
+    file_path = "generated_response.txt" 
+    with open(file_path, "w") as file:
+        file.write(response)
+    process_file_and_write(response)
 
-client = Client('https://ysharma-explore-llamav2-with-tgi.hf.space/--replicas/ppt5s/', verbose=False)
-response = client.predict(prompt, "", 0.9, 512, 0.6, 1.2, api_name='/chat')
-process_file_and_write(response)
+def solve_err(err):
+    file_path = "generated_cmd.txt"  # Replace with the path to your file
+    with open(file_path, "r") as file:
+        generated_response = file.read()
+    prompt="""
+    I used your previous response as shown above. However, I received the error as below.
+    """
+    solution_request = generated_response + '\n' + prompt + '\n' + err
+    client = Client('https://ysharma-explore-llamav2-with-tgi.hf.space/--replicas/ppt5s/', verbose=False)
+    solution_response = client.predict(solution_request, "", 0.9, 512, 0.6, 1.2, api_name='/chat')
+    file_path = "generated_response.txt" 
+    with open(file_path, "w") as file:
+        file.write(solution_response)
+    print(solution_response)
+    process_file_and_write(solution_response)
+
+if __name__ == "__main__":
+    run()
--- run_cmd.py
+++ run_cmd.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+from gale import solve_err 
 
 def process_file_and_write(content):
     matches = re.findall(r"```(.*?)```", content, re.DOTALL)
@@ -17,14 +18,21 @@ def process_file_and_write(content):
     execute_cmd(cmds)
 
 def execute_cmd(cmds):
+    err_occured = False
     for command in cmds:
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
         print(f'Command : {command}')
+        # auto_indentation = ["autopep8 my_script.py --in-place"]
+        # result = subprocess.run(auto_indentation, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
         if result.returncode != 0:
             print("Command failed with error message:")
             print(result.stderr)
+            err = result.stderr
+            err_occured = True
             break;
         print(result.stdout);
+    if(err_occured):
+        solve_err(err)
 
 if __name__ == "__main__":
     content = input("Test Input")
